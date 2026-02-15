493837aff304f134c0399d2b9d4252864c55ae79
Aditya <adi247u@gmail.com>
2026-02-15 16:36:10 +1100

    chore: initialize spring boot project with dependencies

---
+++ src/main/java/com/ad/fx/wallet/FxWalletApplication.java
@@ -0,0 +1,13 @@
+package com.ad.fx.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ad/fx/wallet/FxWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ad.fx.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
