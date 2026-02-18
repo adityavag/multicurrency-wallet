3d951811d948cd51cb7a404fcedbd5aade08f2a4
Aditya <adi247u@gmail.com>
2026-02-18 13:32:07 +1100

    feat: enable Spring Data web support with DTO serialization mode

--- src/main/java/com/ad/fx/wallet/FxWalletApplication.java
+++ src/main/java/com/ad/fx/wallet/FxWalletApplication.java
@@ -2,10 +2,12 @@ package com.ad.fx.wallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class FxWalletApplication {
 
 	public static void main(String[] args) {
